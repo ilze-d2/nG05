bc7ac3f605b4a8447cad6fa61763b618854d2c3b
VoltaireG <92631127+VoltaireG@users.noreply.github.com>
2021-10-19 21:53:19 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Hello World from group nG05! Member: Valters Gailis
+      </Text>
+      <Text style={styles.paragraph}>
+      This is my Valters Gailis first React native application!
+      </Text>
+       <Image style={styles.logo} source={require('./assets/forest.jpg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
