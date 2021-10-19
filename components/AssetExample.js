f615ee56e1faf11a26cf76fc9637189f3df9b31c
maris-o <92183230+maris-o@users.noreply.github.com>
2021-10-19 22:32:15 +0300

    Add files via upload

---
+++ components/AssetExample.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        
+      </Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
