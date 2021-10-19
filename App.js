c4d9c24e2991a68a9374891418858a0b2add8fa9
maris-o <92183230+maris-o@users.noreply.github.com>
2021-10-19 21:38:01 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
@@ -11,44 +11,28 @@ import { Card } from 'react-native-paper';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>
-        Hello world from nG05!
-      </Text>
-       <Text style={styles.paragraph}>
-        Maris Odzulis
-      </Text>
-      <Image style={styles.logo} source={require('./assets/snack-icon.png')} />
       <Text style={styles.paragraph}>
-        This is my first React Native application!
+        Change code in the editor and watch it change on your phone! Save to get a shareable url.
       </Text>
+      <Card>
+        <AssetExample />
+      </Card>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
-    padding: 10,
+    padding: 8,
   },
   paragraph: {
     margin: 24,
-    fontSize: 24,
-    fontWeight: '400',
+    fontSize: 18,
+    fontWeight: 'bold',
     textAlign: 'center',
-  },
-
-  h1: {
-    fontWeight: 500,
-    fontSize: 30,
-
-  },
-  logo: {
-    height: 128,
-    width: 128,
   },
 });
